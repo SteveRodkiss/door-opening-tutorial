481829e5bc321b315505856d6b2b54832f28e519
SRCoder <SteveRodkiss@users.noreply.github.com>
2019-12-09 16:22:27 +1300

    Finished Tutorial with complete code

---
+++ door-opening-tutorial/Assets/DoorTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This will trigger the animations of the door
+/// </summary>
+
+public class DoorTrigger : MonoBehaviour
+{
+    //the animator component
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if this gets triggered open the door
+        if (other.tag == "Player")
+        {
+            animator.SetBool("Open", true);
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetBool("Open", false);
+        }        
+    }
+
+
+}
